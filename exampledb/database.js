a8df395dc6ef8ba7235f57726017a99cf2af7fd5
sakthi-sampathkumar <20cs217sakthi@gmail.com>
2024-01-27 11:27:51 +0530

    aaaaa

--- exampledb/database.js
+++ exampledb/database.js
@@ -4,4 +4,14 @@ const mongo_url='mongodb://localhost:27017/exdb';
 
 exports.connect =()=>{
     
+    mongo.connect(mongo_url,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(()=>{
+        console.log('database connected sucessfully');
+    })
+    .catch((error)=>{
+        console.error('database not connected'+error);
+    })
 }
